72b582ec8fdcf279120e0a42a5dc39e4df48eaba
João Victor França <franca.jv@hotmail.com>
2023-01-03 19:20:33 -0300

    problem 1006

---
+++ problems/1006.js
@@ -0,0 +1,12 @@
+export function problem (lines) {
+    
+    const [A, B, C] = lines;
+    const pesoA = 2;
+    const pesoB = 3;
+    const pesoC = 5;
+
+    const media = ((A * pesoA + B * pesoB + C * pesoC) / (pesoA + pesoB + pesoC)).toFixed(1);
+
+    console.log(`MEDIA = ${media}`);
+
+}
